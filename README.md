8fb295a58a2e9cc3baff725bc941861a9651faea
MatheusDosReis <matheusdrdj@gmail.com>
2018-05-15 16:15:47 -0300

    Now, it's possible to use argument for the filename and beggining and end of interval

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # storia-imoveis-scrapy
+
 Repository of a Web Crawler made to work with [Storia Imóveis](https://www.storiaimoveis.com.br/)
--- crawler/spiders/test.py
+++ crawler/spiders/test.py
@@ -3,28 +3,44 @@ import scrapy
 import csv
 from scrapy.selector import Selector
 
-class GoogleSpider(scrapy.Spider):
+
+class TestSpider(scrapy.Spider):
     name = 'test'
-    allowed_domains=['zapmoveis.com.br']
-    
+    allowed_domains = ['zapmoveis.com.br']
+
+    def __init__(self, filename='new.csv', init=0, end=10, *args, **kwargs):
+        super(TestSpider, self).__init__(*args, **kwargs)
+        self.init = init
+        self.end = end
+        self.filename = filename
+
     def start_requests(self):
-        
-        url= 'https://www.storiaimoveis.com.br/comprar/curitiba-pr'
-        
-        for i in range(0,10):
+        url = 'https://www.storiaimoveis.com.br/comprar/curitiba-pr'
+
+        self.fileLoader()
+
+        for i in range(int(self.init), int(self.end)):
             tmp = url+"?page=%d" % i
             yield scrapy.Request(url=tmp, callback=self.parse)
 
+    def fileLoader(self):
+        try:
+            file = open('results/%s.csv' % self.filename, 'w')
+            self.planilha = csv.writer(
+                file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            self.planilha.writerow(
+                ['Localização', 'Preço', 'Quantidade de Quartos'])
+        except ValueError:
+            self.file = open('results/%s.csv' % self.filename, 'a')
+            self.planilha = csv.writer(
+                file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
     def parse(self, response):
         divs = response.css('div[role="link"]')
-        quartos = divs.css('span[data-ui-tracker="OFFER_BEDROOMS"]::text').extract()
+        quartos = divs.css(
+            'span[data-ui-tracker="OFFER_BEDROOMS"]::text').extract()
         prices = divs.css('div[data-ui-tracker="OFFER_PRICE"]::text').extract()
         locais = divs.css('div span[class="f8 light-gray"]::text').extract()
-        
-        with open('../results/test.csv','a') as csvfile:
-            planilha = csv.writer(csvfile,delimiter=',',quotechar='|',quoting=csv.QUOTE_MINIMAL)
-            
-            for i in range(0,len(quartos)):
-                planilha.writerow([locais[i],prices[i],quartos[i]])
-                
-        
+
+        for i in range(0, len(quartos)):
+            self.planilha.writerow([locais[i], prices[i], quartos[i]])
