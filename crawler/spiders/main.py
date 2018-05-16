d107ebca25f0929f0171251befcee250b23db091
MatheusDosReis <matheusdrdj@gmail.com>
2018-05-15 21:15:38 -0300

    Adding comments and improving the code to minimize erros

--- crawler/spiders/main.py
+++ crawler/spiders/main.py
@@ -29,6 +29,7 @@ class MainSpider(scrapy.Spider):
             file = open('results/%s.csv' % self.filename, 'w')
             self.planilha = csv.writer(
                 file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            # The line below set the headers of the sheet, on the file creation
             self.planilha.writerow(
                 ['Localização', 'Preço', 'Quantidade de Quartos'])
         except ValueError:
@@ -37,11 +38,14 @@ class MainSpider(scrapy.Spider):
                 file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
     def parse(self, response):
-        divs = response.css('div[role="link"]')
-        quartos = divs.css(
-            'span[data-ui-tracker="OFFER_BEDROOMS"]::text').extract()
-        prices = divs.css('div[data-ui-tracker="OFFER_PRICE"]::text').extract()
-        locais = divs.css('div span[class="f8 light-gray"]::text').extract()
+        announces = response.css('div[role="link"]')
 
-        for i in range(0, len(quartos)):
-            self.planilha.writerow([locais[i], prices[i], quartos[i]])
+        for item in announces:
+            rooms = item.css(
+                'span[data-ui-tracker="OFFER_BEDROOMS"]::text').extract_first()
+            price = item.css(
+                'div[data-ui-tracker="OFFER_PRICE"]::text').extract_first()
+            price = price[3:]  # This line removes the 'R$ ' from the string
+            local = item.css(
+                'div span[class="f8 light-gray"]::text').extract_first()
+            self.planilha.writerow([local, price, rooms])
