4aae32acbe5c83887143ed4ed780b287a07eaa81
MatheusDosReis <matheusdrdj@gmail.com>
2018-05-17 02:40:50 -0300

    Working properly and getting coordinates

--- crawler/spiders/imovelweb.py
+++ crawler/spiders/imovelweb.py
@@ -10,8 +10,10 @@ class ImovelwebSpider(scrapy.Spider):
 
     def __init__(self, filename='new', city='goiania', state='go', *args, **kwargs):
         super(ImovelwebSpider, self).__init__()
-        self.urls = []
+        self.url_lotes = []
         self.filename = filename
+        self.city = city
+        self.state = state
 
     def fileLoader(self):
         try:
@@ -20,34 +22,54 @@ class ImovelwebSpider(scrapy.Spider):
                 file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
             # The line below set the headers of the sheet, on the file creation
             self.planilha.writerow(
-                ['ID', 'Bairro', 'Rua', 'Preço'])
+                ['Bairro', 'Rua', 'Preço', 'Latitude', 'Longitude'])
         except ValueError:
             self.file = open('results/%s.csv' % self.filename, 'a')
             self.planilha = csv.writer(
                 file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
     def start_requests(self):
-        url = "http://www.imovelweb.com.br/terrenos-venda-goiania-go"
+        url = "http://www.imovelweb.com.br/terrenos-venda-%s-%s" % (
+            self.city, self.state)
+        urls = []
         self.fileLoader()
-        self.urls.append((url+'.html'))
+        urls.append((url+'.html'))
         for i in range(1, 13):
-            self.urls.append(url+'-pagina-%d.html' % i)
-        for url in self.urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            urls.append(url+'-pagina-%d.html' % i)
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
+
+    def parse_page(self, response):
+        # Get price
+        price = response.css('strong.venta::text').extract_first()
+        price = price[3:]
+
+        lat = response.css('input[name="lat"]::attr("value")').extract_first()
+        lng = response.css('input[name="lng"]::attr("value")').extract_first()
+
+        lat = lat if (lat != None) else ''
+        lng = lng if (lng != None) else ''
+
+        # Get address
+        tmp = response.css(
+            'div.list.list-directions ul li::text').extract_first()
+        if(tmp == None):
+            tmp = ["Desconhecida", "Desconhecido"]
+        else:
+            tmp = tmp.split(',')
+        if len(tmp) > 3:
+            street = tmp[0].strip()
+            neighborhood = tmp[2].strip()
+        else:
+            street = tmp[0].strip()
+            neighborhood = tmp[1].strip()
+
+        self.planilha.writerow([neighborhood, street, price, lat, lng])
+
+    def request_page(self, link):
+        yield scrapy.Request(url=link, callback=self.parse_page, dont_filter=True)
 
     def parse(self, response):
-        items = response.css('li.aviso')
-        for item in items:
-            temp = {}
-            temp['id'] = item.css('::attr("data-aviso")').extract_first()
-            price = item.css(
-                'span.aviso-data-price-value::text').extract_first()
-            temp['price'] = price[3:]
-            street = item.css(
-                'span.aviso-data-location::text').extract_first().strip().replace('\t', '').replace('\n-', '').replace(',', '')
-            temp['street'] = street
-            location = item.css(
-                'span.aviso-data-location span::text').extract_first().split(',')
-            temp['neighborhood'] = location[0]
-            self.planilha.writerow(
-                [temp['id'], temp['neighborhood'], temp['street'], temp['price']])
+        links = response.css('li.aviso::attr("data-href")').extract()
+        for item in links:
+            yield from self.request_page(item)
